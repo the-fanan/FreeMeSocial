0d3c9847b423bbc8797869ab1c611143560cb984
Fanan Dala <fanan.dala@yahoo.com>
2019-05-24 09:32:10 +0100

    feat (auth): added registration form

---
+++ resources/views/layouts/master.blade.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+	<head>
+			<meta charset="utf-8">
+			<meta name="viewport" content="width=device-width, initial-scale=1">
+	
+			<!-- CSRF Token -->
+			<meta name="csrf-token" content="{{ csrf_token() }}">
+			<title>Socially</title>
+
+			<link href="{{ url(mix('css/app.css')) }}" rel="stylesheet" type="text/css">
+			<link href="{{ asset('css/style.css') }}" rel="stylesheet" type="text/css">
+			<link href="{{ asset('css/responsive.css') }}" rel="stylesheet" type="text/css">
+	</head>
+
+	<body>
+		@include('includes.nav')
+
+		@yield('content')
+
+		@include('includes.footer')
+		<!-- Scripts -->
+		<script src="{{ url(mix('js/app.js')) }}" defer></script>
+		@stack('page-scripts')
+	</body>
+</html>
