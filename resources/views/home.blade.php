815476aad2a39b7033bd2318ead29b73e0cbc8f6
Fanan Dala <fanan.dala@yahoo.com>
2019-05-24 16:15:13 +0100

    feat (homepage): added new post form

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,23 +1,64 @@
-@extends('layouts.app')
+@extends('layouts.master')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Dashboard</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
+<div class="container home-container">
+    <div class="row justify-content-between">
+       <div class="col-lg-8 col-md-8 col-sm-8 col-xs-12">
+            <ul class="list-group">
+                <li class="list-group-item list-group-item-very-dark text-white justify-content-between">
+                    <div class="row">
+                        <div class="col-lg-1 col-md-2 col-sm-2 col-2">
+                                <img src="{{ asset('images/avatar.png') }}" class="img-responsive rounded-circle very-small-circle"/>
                         </div>
-                    @endif
+                        <div class="col-lg-11 col-md-10 col-sm-10 col-10">
+                            <form>
+                                <div class="form-group">
+                                    <input class="form-control" placeholder="Title">
+                                </div>
+                                <div class="form-group">
+                                    <button class="file btn btn-primary">
+                                            <input hidden type="file" name="file"/>
+                                            <i class="fa fa-camera"></i>
+                                    </button>
 
-                    You are logged in!
-                </div>
-            </div>
-        </div>
+                                    <button type="submit" class="btn btn-light">
+                                        New Post
+                                    </button>
+                                </div>
+        
+                            </form>
+                        </div>
+                    </div>
+                </li>
+                <li class="list-group-item">
+
+                </li>
+                <li class="list-group-item">
+                    b
+                </li>
+
+                <li class="list-group-item">
+                c
+                </li>
+
+                <li class="list-group-item">
+            d
+                </li>
+            </ul>
+       </div>
+
+       <div class="col-lg-4 col-md-4 col-sm-4 col-xs-12 family-column">
+            <ul class="list-group">
+                    <li class="list-group-item list-group-item-very-dark text-white">
+                      Families
+                    </li>
+            </ul>
+       </div>
     </div>
 </div>
 @endsection
+
+@push('page-scripts')
+<script type="text-javascript">
+</script>
+@endpush
